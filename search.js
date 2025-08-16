e22d271364c90f6558b9db18b3589fdf20de40c2
shubbham-kumarr <shubbhammkumarr@outlook.com>
2025-08-17 00:35:20 +0530

    user-remove-btn-created

--- search.js
+++ search.js
@@ -11,14 +11,30 @@ document.addEventListener("DOMContentLoaded", () => {
   clear.addEventListener(
     'click',
     ()=>{
-   
+      
       localStorage.removeItem("userCards")
-      cardsContainer.innerHTML = "";
-
+      cardsContainer.innerHTML = '<p class="place-self-center  mt-24   text-4xl text-blue-50">Users Removed</p>'
+      setTimeout(()=>{
+        cardsContainer.innerHTML = ""
+      },3000)
      
     }
   )
-  
+ 
+cardsContainer.addEventListener('click', (e) => {
+  if (e.target.closest('.remove-btn')) {
+    const card = e.target.closest('.card');
+    const username = card.dataset.user;
+
+    // Remove from DOM
+    card.remove();
+
+    // Remove from localStorage
+    let savedCards = JSON.parse(localStorage.getItem("userCards")) || [];
+    savedCards = savedCards.filter(html => !html.includes(`data-user="${username}"`));
+    localStorage.setItem("userCards", JSON.stringify(savedCards));
+  }
+});
 
 
 });
@@ -38,21 +54,25 @@ async function fetchAndDisplayUser() {
     const data = await res.json();
 
     const cardHTML = `
-      <div  id="card" class="card grid border border-gray-300 rounded-lg text-white m-2 p-2 bg-blue-500">
-        <img class="h-[100px] w-[100px] place-self-start col-1 rounded-full p-3" src="${data.avatar_url}" alt="github avatar of ${data.login}"/>
-        <div class="col-2 row-1 tracking-wide">
-          <p>${data.name || 'No Name'}</p>
-          <p>${data.bio || 'No bio available'}</p>
-        </div>
-        <p class="p-3 col-1">${data.followers} Followers</p>
-        <p class="p-3">${data.following} Following</p>
-        <div class="row-3 grid">
-        <a href="https://github.com/${data.login}" target="_self"><i class="fa-xl fa-brands fa-github"></i></a>
-         <button id="remove" class="remove-btn col-2"><i class="fas fa-trash fa-fw fa-xl   " style="color: #ffffff;"
-         onmouseover="style='color:red'" onmouseleave="style='color:white'"></i></button>
-        </div>
-      </div>
-    `;
+  <div class="card grid border border-gray-300 rounded-lg text-white m-2 p-2 bg-blue-500" data-user="${data.login}">
+    <img class="h-[100px] w-[100px] place-self-start col-1 rounded-full p-3" src="${data.avatar_url}" alt="github avatar of ${data.login}"/>
+    <div class="col-2 row-1 tracking-wide">
+      <p>${data.name || 'No Name'}</p>
+      <p>${data.bio || 'No bio available'}</p>
+    </div>
+    <p class="p-3 col-1">${data.followers} Followers</p>
+    <p class="p-3">${data.following} Following</p>
+    <div class="row-3 grid">
+      <a href="https://github.com/${data.login}" target="_self">
+        <i class="fa-xl fa-brands fa-github"></i>
+      </a>
+      <button class="remove-btn col-2">
+        <i class="fas fa-trash fa-fw fa-xl" style="color: #ffffff;"></i>
+      </button>
+    </div>
+  </div>
+`;
+
 
     // Show newest first
     cardsContainer.innerHTML = cardHTML + cardsContainer.innerHTML;
